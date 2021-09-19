ddbed87f0987e43f4b16d5069c49f9af61b0a682
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-19 17:17:39 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -17,5 +18,13 @@ app.use ('/api/auth', require('./routes/auth'));
 app.use ('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use( express.static('./client/buid') );
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
