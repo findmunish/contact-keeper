8daa777037313f29d0b2d53c423a1d60fb5fbb81
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-19 18:01:42 +0530

    production related changes to render index.html

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const PORT = process.env.PORT || 5000
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use( express.static('./client/buid') );
+    app.use(express.static('client/buid'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
