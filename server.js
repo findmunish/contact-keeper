e842643e87092be59da3ed75944662e56b44b9e2
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-19 17:48:16 +0530

    app.get('/',) check limited to development build only

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ connectDB();
 // Init middleware
 app.use( express.json({ extended: false }) );
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
+if (process.env.NODE_ENV !== 'production') {
+    app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
+}
 
 // Define routes
 app.use ('/api/users', require('./routes/users'));
@@ -26,5 +28,4 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'index.html')));
 }
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
