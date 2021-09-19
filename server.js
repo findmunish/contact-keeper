7417e165a94068e4e0fad5acbd53f472e839912a
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-19 22:48:55 +0530

    reverted back earlier changes done

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(cors());
 // Init middleware
 app.use( express.json({ extended: false }) );
 
-// if (process.env.NODE_ENV !== 'production') {
-//     app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
-// }
+if (process.env.NODE_ENV !== 'production') {
+    app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
+}
 
 // Define routes
 app.use ('/api/users', require('./routes/users'));
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/buid'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) );
 }
