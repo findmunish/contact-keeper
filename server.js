b0dd9fd729f927f4010656c81514d21218f36a91
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-19 21:44:09 +0530

    sennding the index.html after prod build

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use( express.json({ extended: false }) );
 
 // if (process.env.NODE_ENV !== 'production') {
-    app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
+//     app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact-Keeper API....' }) );
 // }
 
 // Define routes
@@ -27,12 +27,12 @@ app.use ('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000
 
-// // Server static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/buid'));
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/buid'));
 
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) );
-// }
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
